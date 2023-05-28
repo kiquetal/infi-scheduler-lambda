2c0adc88ee851c00d39e83d5e5ec3584d37a2e35
Enrique M. Talavera <kiquetal@gmailcom>
2023-05-28 14:35:07 -0400

    starting the logic

--- handler.py
+++ handler.py
@@ -1,5 +1,8 @@
 import json
 
+import boto3 as boto3
+import botocore.exceptions
+
 
 def hello(event, context):
     body = {
@@ -8,3 +11,23 @@ def hello(event, context):
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
+
+
+def start_instances(event, context):
+    try:
+        ec2 = boto3.client('ec2')
+        ec2.start_instances(InstanceIds=['i-0e9f1e0c4f5b2b5c9'], DryRun=True)
+        return 'Success'
+
+    except botocore.exceptions.ClientError as e:
+        return e.response['Error']['Message']
+
+
+def stop_instances(event, context):
+    try:
+        ec2 = boto3.client('ec2')
+        ec2.stop_instances(InstanceIds=['i-0e9f1e0c4f5b2b5c9'], DryRun=True)
+        return 'Success'
+
+    except botocore.exceptions.ClientError as e:
+        return e.response['Error']['Message']
