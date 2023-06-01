e16b114999e299e08479f08ac8c60bdd10e901bc
Enrique M. Talavera <kiquetal@gmailcom>
2023-06-01 08:50:48 -0400

    fix response from describeInstances

--- lambda/handler.py
+++ lambda/handler.py
@@ -24,18 +24,20 @@ def start_instances(event, context):
             ],
         )
         was_started = False
-        for instance in all_instances['Reservations'][0]['Instances']:
-            all_tags = instance['Tags']    # Get all tags for the instance
-            for tag in all_tags:        # Find the tag with Key = Name
-                if tag['Key'] == 'scheduler':
-                    if tag['Value'] == 'true':
-                        was_started = True
-                        ec2.start_instances(InstanceIds=[instance['InstanceId']], DryRun=True)
-                        print('Started instance: ' + instance['InstanceId'])
+        for reservation in all_instances['Reservations']:
+            for instance in reservation['Instances']:
+                all_tags = instance['Tags']    # Get all tags for the instance
+                for tag in all_tags:        # Find the tag with Key = Name
+                    if tag['Key'] == 'scheduler':
+                        if tag['Value'] == 'true':
+                            was_started = True
+                            ec2.start_instances(InstanceIds=[instance['InstanceId']])
+                            print('Started instance: ' + instance['InstanceId'])
         if not was_started:
             print('No instances to start')
 
     except botocore.exceptions.ClientError as e:
+        print("exception", e)
         return e.response['Error']['Message']
 
 
@@ -51,15 +53,17 @@ def end_instances(event, context):
             ],
         )
         was_stopped = False
-        for instance in all_instances['Reservations'][0]['Instances']:
-            all_tags = instance['Tags']    # Get all tags for the instance
-            for tag in all_tags:        # Find the tag with Key = Name
-                if tag['Key'] == 'scheduler':
-                    if tag['Value'] == 'true':
-                        was_stopped = True
-                        ec2.stop_instances(InstanceIds=[instance['InstanceId']], DryRun=True)
-                        print('Stopped instance: ' + instance['InstanceId'])
+        for reservation in all_instances['Reservations']:
+            for instance in reservation['Instances']:
+                all_tags = instance['Tags']    # Get all tags for the instance
+                for tag in all_tags:        # Find the tag with Key = Name
+                    if tag['Key'] == 'scheduler':
+                        if tag['Value'] == 'true':
+                            was_stopped = True
+                            ec2.stop_instances(InstanceIds=[instance['InstanceId']])
+                            print('Stopped instance: ' + instance['InstanceId'])
         if not was_stopped:
             print('No instances to stop')
     except botocore.exceptions.ClientError as e:
+        print("exception", e)
         return e.response['Error']['Message']
